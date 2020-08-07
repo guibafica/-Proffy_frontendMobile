8170cd726ef37b972532aa309bc7064177cd3ff2
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-08-07 16:08:30 -0300

    CHORE: Estrutura e estiliza Landing Page

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+  useFonts
+} from '@expo-google-fonts/poppins';
+import {
+  Archivo_400Regular,
+  Archivo_700Bold
+} from '@expo-google-fonts/archivo';
+
 import Landing from './src/pages/Landing';
 
 export default function App() {
-  return (
-    <>
-      <Landing />
-      <StatusBar style="auto" />
-    </>
-  );
+  let [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+    Archivo_400Regular,
+    Archivo_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <Landing />
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
